bef4042a116482ea5e67fb41d4be1fb03ebb358a
Ilya Fischhoff <fischhoff@gmail.com>
2019-03-05 01:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too."
   } else if (string.toLowerCase()===string){
-    return 
+    return "I can\'t hear you."
   }
 }
